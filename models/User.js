d3c76bbf2a47f33577e9edda707bad637cc02415
MarynaKindras <maryna.kindras@gmail.com>
2024-05-25 09:22:28 +0300

    Update models and schemas
    Сreated schema and user model for the users collection

---
+++ models/User.js
@@ -0,0 +1,40 @@
+import { Schema, model } from "mongoose";
+import { userSubscriptionValues } from "../utils/constants.js";
+import { emailRegexp } from "../utils/validationPatterns.js";
+import { handleSaveError, setUpdateSettings } from "./hooks.js";
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: emailRegexp,
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+
+    subscription: {
+      type: String,
+      enum: userSubscriptionValues,
+      default: "starter",
+    },
+
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleSaveError);
+userSchema.post("findOneAndUpdate", handleSaveError);
+userSchema.pre("findOneAndUpdate", setUpdateSettings);
+
+const User = model("user", userSchema);
+
+export default User;
