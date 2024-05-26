3333b9263a7b4bc489f2214e5eab01869737639f
MarynaKindras <maryna.kindras@gmail.com>
2024-05-26 13:25:28 +0300

    Update services and adding utils

---
+++ services/userServices.js
@@ -0,0 +1,12 @@
+import User from "../models/User.js";
+import bcryptjs from "bcryptjs";
+
+export const findUser = (filter, fields, options) => User.findOne(filter, fields, options);
+
+export const saveUser = async data => {
+  const hashedPassword = await bcryptjs.hash(data.password, 10);
+  const result = User.create({ ...data, password: hashedPassword });
+  return result;
+};
+
+export const updateUser = (filter, body) => User.findOneAndUpdate(filter, body);
